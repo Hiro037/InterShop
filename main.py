829841dde45273f97f3b2935bfde67c93e433878
Said Magomedov <saidmagomedov037@gmail.com>
2025-06-09 19:27:38 +0500

    Сверстаны веб-страницы (в директории html файлы) Добавлен .gitignore Реализованы GET и POST запросы в модуле main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+# Импорт встроенной библиотеки для работы веб-сервера
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
+
+# Для начала определим настройки запуска
+hostName = "localhost" # Адрес для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        with open('html файлы/Контакты.html', 'r', encoding="utf-8") as file:
+            html_content = file.read()
+
+        self.send_response(200) # Отправка кода ответа
+        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
+        self.end_headers() # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(html_content, "utf-8")) # Тело ответа
+
+    def do_POST(self):
+        """ Метод для обработки входящих POST-запросов """
+        # Чтение длины тела запроса
+        content_length = int(self.headers['Content-Length'])
+        # Чтение тела запроса
+        post_data = self.rfile.read(content_length).decode('utf-8')
+
+        # Парсинг данных (например, из формы)
+        parsed_data = urllib.parse.parse_qs(post_data)
+
+        # Вывод данных в консоль
+        print("Полученные данные из POST-запроса:", parsed_data)
+
+        # Отправка ответа клиенту
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<h1>Данные приняты!</h1>", "utf-16"))
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрах в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
